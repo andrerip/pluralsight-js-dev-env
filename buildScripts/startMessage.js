65538a0bedaefdc2e917b5a3360e4e713fdcb7be
Andre Ripari <andre.ripari@provider-it.com.br>
2018-01-11 11:11:49 -0200

    adicionado o Babel

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,4 @@
-var chalk = require('chalk');
+// var chalk = require('chalk'); //ES5
+import chalk from 'chalk'; //ES6 - O Node não entende
 
 console.log(chalk.green('Starting app in dev mode...'));
